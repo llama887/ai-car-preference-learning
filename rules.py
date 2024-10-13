3a152d60a83504522b5f3ee4d110e50015fdf076
Franklin Yiu <67379586+llama887@users.noreply.github.com>
2024-10-13 00:10:01 -0400

    (feat) new rules

--- rules.py
+++ rules.py
@@ -10,15 +10,28 @@ assert (
 
 def check_rules(segment, total_rules):
     rule_counter = 0
+
+    def evaluate(rule_number, rule_lambda):
+        increment = 1 if rule_lambda() else 0
+        return increment if total_rules >= rule_number else 0
+
     point1 = segment[0].position
     point2 = segment[1].position
     distance = math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-    if total_rules >= 1 and distance > 30:
-        rule_counter += 1
+
+    # Rule 1: Check if the distance between two points is greater than 30
+    rule_counter += evaluate(1, lambda: distance > 30)
 
     left_radar = segment[1].radars[0]
     right_radar = segment[1].radars[4]
-    if total_rules >= 2 and left_radar > right_radar:
-        rule_counter += 1
+
+    # Rule 2: Check if left radar is greater than right radar
+    rule_counter += evaluate(2, lambda: left_radar > right_radar)
+
+    action1 = segment[0].action
+    action2 = segment[1].action
+
+    # Rule 3: Check if actions are not the same
+    rule_counter += evaluate(3, lambda: action1 != action2)
 
     return rule_counter, int(rule_counter == total_rules)
